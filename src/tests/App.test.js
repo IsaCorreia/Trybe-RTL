fd5b2e1ce6b028f65b413f0709000119dcd18a2a
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-02-18 17:54:01 -0300

    Requisito 1 - checa o clique em 'Home' para a rota '/'

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
 // Primeiro commit, let`s go!
 import '@testing-library/jest-dom';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from './renderWithRouter';
@@ -20,21 +21,52 @@ describe('Teste o componente <App.js />', () => {
 
   test(`Teste se a aplicação é redirecionada para a página inicial,
   na URL / ao clicar no link Home da barra de navegação.`, () => {
+    const { history } = renderWithRouter(<App />);
 
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+
+    const homeTitleEl = screen.getByRole('heading', { name: /Encountered pokémons/i });
+    expect(homeTitleEl).toBeInTheDocument();
   });
 
   test(`Teste se a aplicação é redirecionada para a página de About,
   na URL /about, ao clicar no link About da barra de navegação.`, () => {
+    const { history } = renderWithRouter(<App />);
 
+    const aboutLink = screen.getByRole('link', { name: 'About' });
+    expect(aboutLink).toBeInTheDocument();
+
+    userEvent.click(aboutLink);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+
+    const aboutTitleEl = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(aboutTitleEl).toBeInTheDocument();
   });
 
-  test(`Teste se a aplicação é redirecionada para a página de Pokémons Favoritados,
+  test.skip(`Teste se a aplicação é redirecionada para a página de Pokémons Favoritados,
   na URL /favorites, ao clicar no link Favorite Pokémons da barra de navegação.`, () => {
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    const favPkmnLink = screen.getByRole('link', { name: 'Favorite Pokémons' });
 
+    userEvent.click(favPkmnLink);
+    expect(pathname).toBe('/favorites');
   });
 
-  test(`Teste se a aplicação é redirecionada para a página
+  test.skip(`Teste se a aplicação é redirecionada para a página
   Not Found ao entrar em uma URL desconhecida.`, () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/404');
 
+    const notFoundText = screen.getByText(/Page requested not found/i);
+    expect(notFoundText).toBeInTheDocument();
   });
 });
--- src/tests/renderWithRouter.js
+++ src/tests/renderWithRouter.js
@@ -1,11 +1,17 @@
 import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
-const renderWithRouter = (component) => render(
-  <BrowserRouter>
-    {component}
-  </BrowserRouter>,
-);
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  const page = render(
+    <BrowserRouter history={ history }>
+      {component}
+    </BrowserRouter>,
+  );
+
+  return { history, ...page };
+};
 
 export default renderWithRouter;
