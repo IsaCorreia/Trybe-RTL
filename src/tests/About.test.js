76249043fb5cd46d57715ea06a5d017594ca71f0
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-02-18 18:35:32 -0300

    Requisito 2 - Inicio

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import '@testing-library/jest-dom';
+
+describe('Teste o componente <About.js />.', () => {
+  test('Teste se a página contém as informações sobre a Pokédex.', () => { });
+  test('Teste se a página contém um heading h2 com o texto About Pokédex.', () => { });
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => { });
+  test('Teste se a página contém a imagem de uma Pokédex', () => {
+    // const imgURL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  });
+});
