92cbe5816392c0f1dac39fd6630c581d5bf332eb
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-02-21 17:13:19 -0300

    Requisito 5 - Inicio

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,7 @@
-test('', () => {});
+describe('5. Teste o componente <Pokedex.js />', () => {
+  test('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => { });
+  test('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo pokémon é clicado.', () => { });
+  test('Teste se é mostrado apenas um Pokémon por vez.', () => { });
+  test('Teste se a Pokédex tem os botões de filtro.', () => { });
+  test('Teste se a Pokédex contém um botão para resetar o filtro', () => { });
+});
