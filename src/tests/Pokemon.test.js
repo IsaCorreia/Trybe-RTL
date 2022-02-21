bc7c836c6d55ca5d832d17efa660ef246124caab
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-02-21 19:26:30 -0300

    Requisito 6 - Inicio

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+describe('6. Teste o componente <Pokemon.js />', () => {
+  test(`Teste se é renderizado um card com as informações
+  de determinado pokémon.`, () => { });
+
+  test(`Teste se o card do Pokémon indicado na Pokédex contém
+  um link de navegação para exibir detalhes deste Pokémon.
+  O link deve possuir a URL /pokemons/<id>,
+  onde <id> é o id do Pokémon exibido;`, () => { });
+
+  test(`Teste se ao clicar no link de navegação do Pokémon,
+  é feito o redirecionamento da aplicação para
+  a página de detalhes de Pokémon.`, () => { });
+
+  test(`Teste também se a URL exibida no navegador muda para /pokemon/<id>,
+  onde < id > é o id do Pokémon cujos detalhes se deseja ver; `, () => {});
+
+  test('Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {});
+});
