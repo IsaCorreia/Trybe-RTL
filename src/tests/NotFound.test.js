e88efac2a3d5de16de215bef225903bd97ca719d
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-02-21 16:53:47 -0300

    Requisito 4 - Inicio

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,5 @@
-test('', () => {});
+describe('4. Teste o componente <NotFound.js />', () => {
+  test(`Teste se página contém um heading h2
+  com o texto Page requested not found 😭; `, () => {});
+  test('Teste se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {});
+});
