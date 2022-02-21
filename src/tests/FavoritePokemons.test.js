19e25ae208afd551eceed27ce802be035d7e01a9
Isabela Correia <isabela.correiasilva00@gmail.com>
2022-02-21 15:02:10 -0300

    Requisito 3 - Inicio

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,6 @@
-test('', () => {});
+import '@testing-library/jest-dom';
+
+describe('3. Teste o componente <FavoritePokemons.js />', () => {
+  test('é exibido na tela a mensagem No favorite pokemon found, se a pessoa não tiver pokémons favoritos.', () => { });
+  test('é exibido todos os cards de pokémons favoritados.', () => { });
+});
